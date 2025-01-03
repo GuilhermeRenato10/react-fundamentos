cbe46286787fc5ecc7ffb964dddb07d00d19ed98
Guilherme Renato Terra De Macedo <guirenato1181@gmail.com>
2025-01-02 21:01:15 -0300

    Adicionando arquivos iniciais

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const categoty = 'Posts da semana';
+
+function App() {
+    return(
+        <>
+            <h1>JStack´s Blog</h1>
+            <h2>{categoty}</h2>
+        </>
+    ) ;
+}
+
+export default App;
